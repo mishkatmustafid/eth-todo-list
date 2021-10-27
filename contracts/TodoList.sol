0c4922ff17b09ba5088ba8c9a81efda396241a19
Mishkat Mustafid <mishkatmustafid@gmail.com>
2021-10-27 21:46:01 +0600

    Project Setup Complete

---
+++ contracts/TodoList.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: ISC
+pragma solidity ^0.5.0;
+
+// The error shows in vscode is actually false alarm for the current version.
+// The real reason for the error is that the compiler is not able to resolve
+
+contract TodoList{
+  uint public taskCount = 0;
+}
